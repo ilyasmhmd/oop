f6869f00c0c5d88676a3522f88f5348e089f56e7
= <=ilyasmahamed7@gmail.com>
2023-09-23 13:59:21 +0100

    first commit

---
+++ objects.js
@@ -0,0 +1,14 @@
+   let player1 = {
+ firstName : "Iyas",
+ lastName: "Mahamed",
+ age : 13 
+}
+ let player2 = {
+ firstName : "Yonis",
+ lastName : " Yusuf",
+ age : 11
+}
+
+
+console.log(player1.firstName)
+console.log(player2.lastName)
